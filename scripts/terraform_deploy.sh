b59b6b9403f8423c45ee8edae77148c5bc7ce0be
durvesh <durveshyadav3604@gmail.com>
2026-09-18 07:08:13 +0530

    Add two script for terraform apply/destroy

---
+++ scripts/terraform_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "��� Starting Terraform Deployment..."
+
+cd terraform/main
+
+terraform init
+terraform validate
+terraform plan -out=tfplan
+terraform apply -auto-approve tfplan
+
+echo "✅ Terraform deployment completed successfully."
